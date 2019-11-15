f8601cfbb85f5c659350efb7dc00fc878c3d2ba9
omarguabaceo <omarguabaceo@gmail.com>
2019-11-14 20:18:54 -0400

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,41 @@
+body {
+  background: #567;
+  color: #036;
+  font-family: "Andale mono", monospace;
+  font-size: 14px; }
+
+article {
+  background: white;
+  width: 76px;
+  margin: 0 auto;
+  padding: 40px;
+  box-sizing: border-box;
+  position: relative;
+  z-index: -2; }
+
+header {
+  border-radius: 100%;
+  height: 200px;
+  width: 200px;
+  line-height: #036;
+  text-align: center;
+  color: white;
+  background: #567;
+  margin: 0 auto; }
+
+h1 {
+  font-size: 16px; }
+
+footer {
+  border-radius: 100%;
+  height: 150px;
+  width: 150px;
+  line-height: #036;
+  text-align: center;
+  color: white;
+  background: #567;
+  margin: 0 auto;
+  font-size: 14px -3;
+  z-index: -4;
+  position: relative;
+  margin-top: 60px; }
